bb5eb1e22450ba293aa338c251cb6a045047392c
Tyler Tracy <me@tylertracy.com>
2019-04-23 23:21:24 -0500

    Cleaned up some code and make queue load better

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,12 @@ import { PlayerComponent } from "./listen/player/player.component";
 import { ListenComponent } from "./listen/listen.component";
 import { HostFindComponent } from "./host-find/host-find.component";
 import { VibeSettingsComponent } from "./vibe-settings/vibe-settings.component";
-import { SigninComponent } from "./account/signin/signin.component";
 import { AccountComponent } from "./account/account.component";
 import { HomePageComponent } from "./pages/home-page/home-page.component";
 
 const routes: Routes = [
   { path: "account", component: AccountComponent },
   { path: "host", component: PlayerComponent },
-  { path: "login", component: SigninComponent },
   { path: "queue/:id", component: ListenComponent },
   { path: "find", component: HostFindComponent },
   { path: "vibe", component: VibeSettingsComponent },
