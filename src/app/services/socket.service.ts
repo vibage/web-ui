868d407cf69aa1a1964722d04277bb4438c14dcf
Tyler Tracy <me@tylertracy.com>
2019-09-24 18:21:43 -0500

    Provided the Socket service in root

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from "@angular/core";
 import { Socket } from "ng-socket-io";
 import { FeatureFlagService } from "./feature-flags.service";
 
-@Injectable()
+@Injectable({
+  providedIn: "root"
+})
 export class SocketService extends Socket {
   constructor(features: FeatureFlagService) {
     super({ url: features.apiUrl, options: {} });
